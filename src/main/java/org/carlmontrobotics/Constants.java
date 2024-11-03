c68abd96ff26105f27d1cd26d5ece3b69943d49f
Niosocket11 <107586975+Niosocket11@users.noreply.github.com>
2024-11-03 15:30:15 -0800

    Initialize talyaPractice

---
+++ src/main/java/org/carlmontrobotics/Constants.java
@@ -0,0 +1,24 @@
+package org.carlmontrobotics;
+
+public final class Constants {
+    // public static final class Drivetrain {
+    //     public static final double MAX_SPEED_MPS = 2;
+    // }
+    public static final class SHOOTER {
+        // public static final double kP = 0.0001;
+        // public static final double kI = 0;
+        // public static final double kD = 0;
+        // public static final double kS = 0.29753;
+        // public static final double kV = 0.077913;
+        // public static final double kA = 0.05289;
+
+    }
+    public static final class OI {
+        public static final class Driver {
+            public static final int port = 0;
+        }
+        // public static final class Manipulator {
+        //     public static final int port = 1;
+        // }
+    }
+}
